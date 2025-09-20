1b297c6fb9648cc2686fa55b0823ef6b577d3249
anshulneos-boop <anshul.neos@gmail.com>
2025-09-20 05:59:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 import mean_var_std
 
-#testing it using different values
-#test value one 
+# Test 1
 test_lst = [0,1,2,3,4,5,6,7,8]
 print("Result of test list one")
-test_one = mean_var_std.calculate(test_lst)
-print(test_one)
+print(mean_var_std.calculate(test_lst))
 print("--------------------------------------------------------------")
 
-#test value two 
+# Test 2
 test_lst = [2,6,2,8,4,0,1,5,7]
 print("Result of test list two")
-test_one = mean_var_std.calculate(test_lst)
-print(test_one)
-
-
+print(mean_var_std.calculate(test_lst))
 print("--------------------------------------------------------------")
-#test value three 
+
+# Test 3
 test_lst = [9,1,5,3,3,3,2,9,0]
 print("Result of test list three")
-test_one = mean_var_std.calculate(test_lst)
-print(test_one)
-
-
+print(mean_var_std.calculate(test_lst))
 print("--------------------------------------------------------------")
-#test value four 
-test_lst = [2,6,2,8,4,0,1,]
+
+# Test 4 (invalid length - will raise ValueError)
+test_lst = [2,6,2,8,4,0,1]
 print("Result of test list four")
-test_one = mean_var_std.calculate(test_lst)
-print(test_one)
+try:
+    print(mean_var_std.calculate(test_lst))
+except ValueError as e:
+    print(e)  # prints "List must contain nine numbers."
